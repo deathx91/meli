3b00daef66a92a0087061e3ac7fc2fe1cba95f31
Agustin Garcia <medeleven@hotmail.com>
2020-10-21 17:37:43 -0300

    Carga inicial

---
+++ Controllers/TopSecretController.cs
@@ -0,0 +1,24 @@
+﻿using Api.Models;
+using Api.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Api.Controllers
+{
+    public class TopSecretController : ApiController
+    {
+        private TopSecretRepository topSecretRepository;
+        public TopSecretController()
+        {
+            topSecretRepository = new TopSecretRepository();
+        }
+        public TopSecret[] Get(int Distance)
+        {
+            return topSecretRepository.GetSecret(Distance);
+        }
+    }
+}
---
+++ Services/TopSecretRepository.cs
@@ -0,0 +1,28 @@
+﻿using Api.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Api.Services
+{
+    public class TopSecretRepository
+    {
+        public TopSecret[] GetSecret(int Distance)
+        {
+            return new TopSecret[]
+            {
+            new TopSecret
+            {
+                Id = 1,
+                Name = "Glenn Block"
+            },
+            new TopSecret
+            {
+                Id = 2,
+                Name = "Dan Roth"
+            }
+            };
+        }
+    }
+}
